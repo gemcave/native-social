c3241c9ac86d869347194d97b9613f00fe1f9a32
yerowyn <yerowyn@gmail.com>
2020-06-18 11:28:23 +0600

    add and edit: main index for react-navigation

---
+++ App/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import Feed from './screens/Feed';
+import Thread from './screens/Thread';
+import NewStatus from './screens/NewStatus';
+
+import { Button } from './components/Button';
+import { client } from './graphql/client';
+
+const AppStack = createStackNavigator();
+const ModalStack = createStackNavigator();
+
+const AppNavigatorStack = () => {
+  return (
+    <AppStack.Navigator
+      initialRouteName="Feed"
+      screenOptions={{ title: 'Home' }}
+    >
+      <AppStack.Screen
+        name="Feed"
+        component={Feed}
+        options={({ navigation, route }) => ({
+          title: 'Home',
+          headerTintColor: '#fff',
+          // headerStyle: {
+          //   backgroundColor: route.params.color || '#fff',
+          //   borderBottomColor: route.params.color || '#fff',
+          // },
+          headerRight: () => (
+            <Button
+              text="New Status"
+              onPress={() => navigation.navigate('NewStatus')}
+            />
+          ),
+        })}
+      />
+      <AppStack.Screen name="Thread" component={Thread} />
+    </AppStack.Navigator>
+  );
+};
+
+const ModalNavigatorStack = () => {
+  return (
+    <NavigationContainer>
+      <ModalStack.Navigator mode="modal" headerMode="none">
+        <ModalStack.Screen name="App" component={AppNavigatorStack} />
+        <ModalStack.Screen
+          name="NewStatus"
+          component={NewStatus}
+          options={({ route }) => ({
+            title: 'New Status',
+          })}
+        />
+      </ModalStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const App = ModalNavigatorStack;
+
+export default () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
